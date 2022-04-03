1cf0338ef466296ab1d9e136e9feb870a3d64a06
Matthieu Coudron <mcoudron@hotmail.com>
2022-04-03 23:44:20 +0200

    refactor package.nix

--- rofi-hoogle-plugin/package.nix
+++ rofi-hoogle-plugin/package.nix
@@ -1,19 +1,21 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.stdenv.mkDerivation rec {
+{ stdenv, lib, pkg-config, glib, libnotify, makeWrapper, rofi-unwrapped, hs-hoogle-query, 
+cairo
+, xdg-utils }:
+stdenv.mkDerivation rec {
   pname = "rofi-hoogle-plugin";
   version = "0.0.1";
   src = ./src;
 
-  nativeBuildInputs = with pkgs; [
+  nativeBuildInputs = [
     pkg-config
     glib
     libnotify
     makeWrapper
     rofi-unwrapped
-    hs-hoogle-query
+    # hs-hoogle-query
   ];
 
-  buildInputs = with pkgs; [
+  buildInputs = [
     glib
     libnotify
     makeWrapper
@@ -28,7 +30,7 @@ pkgs.stdenv.mkDerivation rec {
     make install -e INSTALL_ROOT=$out
   '';
 
-  meta = with pkgs.lib; {
+  meta = with lib; {
     description = "Search Hoogle from Rofi";
     homepage = "https://github.com/rebeccaskinner/rofi-hoogle";
     license = licenses.bsd3;
